987510789e91fa37342a3d137b97d0bf79bda47d
Jankish <danieljankovic89@gmail.com>
2015-12-10 14:45:16 +0100

    Further work done with GUI. JFileChooser implemented and JList. A way to store file and name which are suppose to be proccessed is next

--- GUI.java
+++ GUI.java
@@ -1,44 +1,84 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JList;
 
 public class GUI extends JFrame{
 	
-	JButton button1;
-
+	JButton confirm;
+	JButton abort;
+	JList list;
+	DefaultListModel model;
+	JFileChooser fileChooser;
+	
 	public GUI() {
-		setSize(800,800);
+		setSize(750,500);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		Dimension dim = tk.getScreenSize();
 		setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 		setResizable(false);	
 		setTitle("GRAB v1.0");
+		setDefaultLookAndFeelDecorated(true);
 
-		//Panel
-		JPanel panel = new JPanel();
 
-		//Label
-		JLabel label1 = new JLabel("Tell me something");
+		//GridBagLayout
+		GridLayout gridLayout = new GridLayout();
 		
-		//TextField
-		JTextField field1 = new JTextField("", 15);
-		field1.setToolTipText("Put information here");
+		//Top Panel
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BorderLayout());
+
+		//South Panel
+		JPanel southPanel = new JPanel();
+
+		//Center Panel
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(gridLayout);
+
+		//File Chooser
+		fileChooser = new JFileChooser();
+	      	//fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+	      	fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		FileNameExtensionFilter filter = new FileNameExtensionFilter( "xml","kos");
+		fileChooser.setApproveButtonText("Välj");
+		//fileChooser.addChoosableFileFilter(filter);
+		//fileChooser.setFileFilter(filter);
+		//centerPanel.add(fileChooser, BorderLayout.WEST);
+		FileListener fileList = new FileListener();
+		fileChooser.addActionListener(fileList);
+		centerPanel.add(fileChooser);
 		
 		//Button
-		button1 = new JButton("Send");
-		button1.setToolTipText("Put information here");
-		ButtonListener buttonL1 = new ButtonListener();
-		button1.addActionListener(buttonL1);
+		confirm = new JButton("Skapa excel");
+		ButtonListener confirmList = new ButtonListener();
+		confirm.addActionListener(confirmList);
+
+		abort = new JButton("Avbryt");
+		ButtonListener abortList = new ButtonListener();
+		abort.addActionListener(abortList);
+		
+		//List model
+		model = new DefaultListModel();
+
+		//List
+		list = new JList(model); 
+		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.setVisibleRowCount(-1);
+		JScrollPane pane = new JScrollPane(list);
+		centerPanel.add(pane);
 
 		//Add to Panel
-		panel.add(field1);
-		panel.add(label1);
-		panel.add(button1);
+		southPanel.add(abort);
+		southPanel.add(confirm);
 		
+		topPanel.add(centerPanel, BorderLayout.NORTH);	
+		topPanel.add(southPanel, BorderLayout.SOUTH);
+
 		//Add to frame
-		add(panel);
+		add(topPanel);
 
 		setVisible(true);
 
@@ -47,14 +87,26 @@ public class GUI extends JFrame{
 	private class ButtonListener implements ActionListener{
 		
 		public void actionPerformed(ActionEvent e) {
-			if (e.getSource() == button1) {
-				//do Something
-	//			field1.append("Button clicked");
+			if (e.getSource() == confirm) {
 				e.getSource().toString();
-				System.out.println("Button Pressed");
+				System.out.println("Confirm Pressed");
+			} else if(e.getSource() == abort) {
+				System.out.println("Abort Pressed");
+				System.exit(0);
 			}
 		}
 
+	}
+	
+	private class FileListener implements ActionListener {
+		
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser chooser = (JFileChooser) e.getSource();
+			if (e.getActionCommand().equals(JFileChooser.APPROVE_SELECTION)) {
+				//model.add(fileChooser.getSelectedFile());
+				System.out.println("File selected: " + fileChooser.getSelectedFile().getName());
+			}
+		}
 	}
 }
 
