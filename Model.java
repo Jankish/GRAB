d7acb1da4c88f5d45672d65a11eaa10b47811835
Jankish <danieljankovic89@gmail.com>
2015-12-14 13:01:01 +0100

    no message, cleaning

--- Model.java
+++ Model.java
@@ -51,7 +51,7 @@ public class Model {
 			for (int i = 4; i <= outer; i+=9){
 				Data d = new Data(order.get(i+1), order.get(i+3), order.get(i+4), order.get(i+5), order.get(i+7));
 				dataList.add(d);
-			/*	try {
+				/*	try {
 					System.out.println(order.get(i+5));
 					number = format.parse(order.get(i+5));
 					System.out.println("Number is " + number);
@@ -61,11 +61,11 @@ public class Model {
 					total = number.doubleValue();
 					Data d = new Data(order.get(i), Integer.parseInt(order.get(i+1)), Integer.parseInt(order.get(i+2)), order.get(i+3), order.get(i+4), price, total);
 					dataList.add(d);
-				} catch (ParseException numExcep) {
+					} catch (ParseException numExcep) {
 					numExcep.printStackTrace();
 					System.exit(1);
-				}
-				**/
+					}
+				 **/
 			}
 			printDataList(dataList);
 		} catch (FileNotFoundException found) {
@@ -81,10 +81,10 @@ public class Model {
 	}
 
 	public void createExcel(String file) {
-		
+
 		String filename = createPath(file);
 		System.out.println(filename);
-		
+
 		try {
 			WorkbookSettings ws = new WorkbookSettings();
 			ws.setLocale(new Locale("sv", "SE"));
@@ -111,19 +111,126 @@ public class Model {
 	}
 
 	private static void writeDataSheet(WritableSheet s) {
-	
+
+		/* Format the Font */
+		WritableFont wf = new WritableFont(WritableFont.ARIAL, 12, WritableFont.NO_BOLD);
+		WritableCellFormat cf = new WritableCellFormat(wf);
+		cf.setWrap(true);
+
+		/* Creates Label and writes date to one cell of
+		 * sheet*/
+		Label l = new Label(0,0,"Date",cf);
+		s.addCell(l);
+		WritableCellFormat cf1 = 
+			new WritableCellFormat(DateFormats.FORMAT9);
+
+		DateTime dt = 
+			new DateTime(0,1,new Date(), cf1, DateTime.GMT);
+
+		s.adCell(dt);
+
+		/* Creates Label and writes
+		 * float number to one cell
+		 * of sheet*/
+		l = new Label(2,0,"Float", cf);
+		s.addCell(l);
+		WritableCellFormat cf2 = new WritableCellFormat(NumberFormats.FLOAT);
+		Number n = new Number(2,1,3.1415926535,cf2);
+		s.addCell(n);
+
+		n = new Number(2,2,-3.1415926535, cf2);
+		s.addCell(n);
+
+		/* Creates
+		 * Label
+		 * and
+		 * writes
+		 * float
+		 * number
+		 * upto
+		 * 3 
+		 *        decimal
+		 *        to
+		 *        one
+		 *        cell
+		 *        of
+		 *        sheet
+		 *        */
+		l = new Label(3,0,"3dps",cf);
+		s.addCell(l);
+		NumberFormat dp3 = new NumberFormat("#.###");
+		WritableCellFormat dp3cell = new WritableCellFormat(dp3);
+		n = new Number(3,1,3.1415926535,dp3cell);
+		s.addCell(n);
+
+		/* Creates
+		 * Label
+		 * and
+		 * adds
+		 * 2
+		 * cells
+		 * of
+		 * sheet*/
+		l = new Label(4, 0, "Add 2 cells",cf);
+		s.addCell(l);
+		n = new Number(4,1,10);
+		s.addCell(n);
+		n = new Number(4,2,16);
+		s.addCell(n);
+		Formula f = new Formula(4,3, "E1+E2");
+		s.addCell(f);
+
+		/* Creates
+		 * Label
+		 * and
+		 * multipies
+		 * value
+		 * of
+		 * one
+		 * cell
+		 * of
+		 * sheet
+		 * by
+		 * 2*/
+		l = new Label(5,0, "Multipy by 2",cf);
+		s.addCell(l);
+		n = new Number(5,1,10);
+		s.addCell(n);
+		f = new Formula(5,2, "F1 * 3");
+		s.addCell(f);
+
+		/* Creates
+		 * Label
+		 * and
+		 * divide
+		 * value
+		 * of
+		 * one
+		 * cell
+		 * of
+		 * sheet
+		 * by
+		 * 2.5
+		 * */
+		l = new Label(6,0, "Divide",cf);
+		s.addCell(l);
+		n = new Number(6,1, 12);
+		s.addCell(n);
+		f = new Formula(6,2, "F1/2.5");
+		s.addCell(f);
 	}
+}
 
 
-	public void printList(ArrayList<String> list) {
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
-		}
+public void printList(ArrayList<String> list) {
+	for (int i = 0; i < list.size(); i++) {
+		System.out.println(list.get(i));
 	}
+}
 
-	public void printDataList(ArrayList<Data> list) {
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
-		}
+public void printDataList(ArrayList<Data> list) {
+	for (int i = 0; i < list.size(); i++) {
+		System.out.println(list.get(i));
 	}
+}
 }
